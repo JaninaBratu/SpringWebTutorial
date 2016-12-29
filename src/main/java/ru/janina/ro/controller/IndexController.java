fbe25cf852194b747ba2fbd8c0ee05c18b70fc87
Janina <janina.bratu@gmail.com>
2016-12-30 00:34:13 +0200

    Spring root context + Adjust package and controller name

--- src/main/java/ru/janina/ro/controller/IndexController.java
+++ src/main/java/ru/janina/ro/controller/IndexController.java
@@ -1,4 +1,4 @@
-package cz.jiripinkas.jba.controller;
+package ru.janina.ro.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
