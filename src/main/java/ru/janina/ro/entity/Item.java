0c88167cc48fa06e65595f7f44caf95647c10edb
Janina <janina.bratu@gmail.com>
2017-01-01 23:06:57 +0200

    JPA Entities

---
+++ src/main/java/ru/janina/ro/entity/Item.java
@@ -0,0 +1,72 @@
+package ru.janina.ro.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Item {
+
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String name;
+	
+	private String description;
+	
+	@Column(name = "published_date")
+	private Date publishedDate;
+	
+	@ManyToOne
+	@JoinColumn(name = "blog_Id")
+	private Blog blog;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getPublishedDate() {
+		return publishedDate;
+	}
+
+	public void setPublishedDate(Date publishedDate) {
+		this.publishedDate = publishedDate;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	private String link;
+}
