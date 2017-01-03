f9294db8247223ca6e7503271f5f68f1ec3abeef
Janina <janina.bratu@gmail.com>
2017-01-03 21:46:41 +0200

    Hibernate Transactional - second method

--- src/main/java/ru/janina/ro/repository/ItemRepository.java
+++ src/main/java/ru/janina/ro/repository/ItemRepository.java
@@ -1,9 +1,13 @@
 package ru.janina.ro.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import ru.janina.ro.entity.Blog;
 import ru.janina.ro.entity.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Integer>{
 
+	List<Item> findByBlog(Blog blog);
 }
--- src/main/java/ru/janina/ro/service/UserService.java
+++ src/main/java/ru/janina/ro/service/UserService.java
@@ -4,8 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import ru.janina.ro.entity.Blog;
+import ru.janina.ro.entity.Item;
 import ru.janina.ro.entity.User;
+import ru.janina.ro.repository.BlogRepository;
+import ru.janina.ro.repository.ItemRepository;
 import ru.janina.ro.repository.UserRepository;
 
 @Service
@@ -15,6 +20,12 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private BlogRepository blogRepository;
+	
+	@Autowired
+	private ItemRepository itemRepository;
+	
 	public List<User> findAll()
 	{
 		return userRepository.findAll();
@@ -23,5 +34,18 @@ public class UserService {
 	public User findOne(int id) 
 	{
 		return userRepository.findOne(id);
+	}
+
+	@Transactional
+	public User findOneWithBlogs(int id) {
+		User user = findOne(id);
+		List<Blog>blogs = blogRepository.findByUser(user);
+		for(Blog blog:blogs)
+		{
+			List<Item> items = itemRepository.findByBlog(blog);
+			blog.setItems(items);
+		}
+		user.setBlogs(blogs);
+		return user;
 	}
 }
