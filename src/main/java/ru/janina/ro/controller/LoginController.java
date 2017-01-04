e31c1c139e2491fce46a9d5049a386e41c80d3fe
Janina <janina.bratu@gmail.com>
2017-01-04 18:22:57 +0200

    Spring Security login page

---
+++ src/main/java/ru/janina/ro/controller/LoginController.java
@@ -0,0 +1,15 @@
+package ru.janina.ro.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+	
+	@RequestMapping("/login")
+	public String login()
+	{
+		return "login";
+	}
+}
